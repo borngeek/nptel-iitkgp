33f9cf22fabaefbbcc1b482f5f9c15480332aae4
borngeek <bekar.solutions@gmail.com>
2017-08-22 09:01:20 +0530

    displayVideo.php & lectureList altered. dbSubjectList.json - to be altered.

--- error.php
+++ error.php
@@ -1,5 +1,65 @@
-<?php
-
-if($_GET["status"] === "404"){
-	var_dump($_SERVER); 
-}
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Display Video</title>
+  <style>
+  .container{
+    position:absolute;
+    left:0px;
+    top:0px;
+    right:0px;
+  }
+  #header{
+    height:50px;
+    background-color:#FFF;
+    border-bottom: 1px #000 solid;
+    padding:5px 0px 5px 5px;
+  }
+  #header img{
+    height: 45px;
+    background-color:#FFF;
+    border-right: 1px #000 solid;
+    display: inline-block;
+  }
+  #header .heading{
+    display: inline-block;
+    font-size: 12pt;
+    vertical-align: top;
+  }
+  #header .heading h4,h6{
+    margin: 0px !important;
+    line-height: 20pt;
+  }
+  #header .heading h6{
+    border-top: 1px #000 solid;
+  }
+  .message{
+    margin-top: 75px;
+    text-align: center;
+  }
+  </style>
+ </head>
+ <body>
+  <div class="container">
+   <div id="header">
+    <img src="/nptel/img/banner500x100.png">
+    <div class="heading">
+		<h4><?php
+			if($_GET["status"] === "404"){
+			  echo "Error 404: File Not Found";
+		    }elseif($_GET["status"] === "403"){
+			  echo "Error 403: Forbidden";
+		    }elseif($_GET["status"] === "500"){
+			  echo "Error 500: Server Error";
+		    }
+		  ?></h4>
+		<h6></h6>
+    </div>
+   </div>
+  </div>
+  <div class="message">
+   Seems like we don't have the file you have requested for.
+  </div>
+ </body>
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,36 @@
 <?php
-echo $_GET['a']."<br>";
-echo $_GET['b']."<br>";
-echo $_GET['c'];
+
+$dir = scandir("json_db", SCANDIR_SORT_ASCENDING);
+$dscArray = array();
+foreach($dir as $filename){
+	if(preg_match("/dbDisciplineList[0-9]*\.json/", $filename, $mDl)){
+		$dscArray = array_merge($dscArray, json_decode(file_get_contents ("json_db/".$mDl[0]), true));
+	}
+}
+
+if(!isset($_GET["dscId"])){
+  foreach ( $dscArray as $dsc ){
+    echo ("<a href='".$dsc["id"]."'>".$dsc["Name"]."</a><br>");
+  }
+}elseif(isset($_GET["dscId"])){
+  $foundFlg = 0;
+  foreach ( $dscArray as $dsc ){
+    if ($_GET["dscId"] === $dsc["id"]){$foundFlg = 1;break;}
+  }
+  if($foundFlg == 1){
+    $subjArray = array();
+    $lecArray = array();
+    foreach($dir as $filename){
+      if(preg_match("/dbSubjectList[0-9]*\.json/", $filename, $mSl)){
+        $subjArray = array_merge($subjArray, json_decode(file_get_contents ("json_db/".$mSl[0]), true));
+//      }elseif(preg_match("/dbLectureList[0-9]*\.json/", $filename, $mLl)){
+//        $lecArray = array_merge($lecArray, json_decode(file_get_contents ("json_db/".$mLl[0]), true));
+	  }
+    }
+    foreach ( $subjArray as $subj ){
+      if ($subj["dscId"] === $dsc["id"]){
+		echo ("<a href='DisplayVideo/".$subj["id"]."'>".$subj["subjName"]."</a><br>");
+	  }
+    }
+  }
+}
